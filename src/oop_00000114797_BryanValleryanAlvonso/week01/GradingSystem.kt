2dbd2a37854e3b9a36b48c01d0360321f3de9c46
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-02-09 20:26:23 +0700

    week01: add calculateStatus function

--- src/oop_00000114797_BryanValleryanAlvonso/week01/GradingSystem.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week01/GradingSystem.kt
@@ -1,5 +1,7 @@
 package oop_00000114797_BryanValleryanAlvonso.week01
 
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name: String = "John Thor"
     val score: Int = 80
@@ -13,4 +15,5 @@ fun main() {
         else -> "D"
     }
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
