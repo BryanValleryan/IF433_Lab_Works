7bb33b4d0b7a5c860c334d128bd2bd33a5c31d1c
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-02-10 19:25:45 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000114797_BryanValleryanAlvonso/week01/GameStore.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week01/GameStore.kt
@@ -2,5 +2,6 @@ package oop_00000114797_BryanValleryanAlvonso.week01
 
 fun main() {
     val gameTitle = "Object Oriented Program"
-    val price = 555555
+    val price = 600000
 }
+fun calculateDiscount(price: Int) = if (price > 500000) price - ((price * 20) / 100) else price - ((price * 10) / 100)
