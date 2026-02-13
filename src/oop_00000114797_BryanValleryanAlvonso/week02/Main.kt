c56c7a1ad567b740209817186112093b3d5aac0b
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-02-13 15:58:23 +0700

    week02: implement selection logic for constructors

--- src/oop_00000114797_BryanValleryanAlvonso/week02/Main.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week02/Main.kt
@@ -11,18 +11,28 @@ fun main() {
     val name = scanner.nextLine()
 
     print("Massukan NIM (Wajib 5 karakter): ")
-    val nim = scanner.nextLine()
+    val nim = scanner.next()
 
     scanner.nextLine()
 
     if (nim.length != 5) {
         print("Error: pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan jurusan: ")
-        val major = scanner.nextLine()
+        print("Pilih jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-        val s1 = Student(name, nim, major)
-        println("Status: pendaftaran  selesai.")
+        if (type == 1){
+            print("masukkan jurusan: ")//pilih jurusan diletakkan disini, karena aneh jika pilih jalur umum tetapi sudah memiliki jurusan
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        } else if (type == 2){
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
 
     }
 }
