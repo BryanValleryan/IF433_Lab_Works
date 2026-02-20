f99c7ab82050eae2b54f9ec93c6d8bcf0b4fae5b
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-02-20 19:52:00 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000114797_BryanValleryanAlvonso/week03/Employee.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000114797_BryanValleryanAlvonso.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+    var(value) {
+        println("mencoba set gaji ke: $value")
+
+        this.salary = value
+    }
 }
