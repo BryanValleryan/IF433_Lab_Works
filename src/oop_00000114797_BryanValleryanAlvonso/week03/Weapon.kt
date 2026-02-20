b99c327dbcf0e92e1c13bc3305cd76e7d012d062
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-02-20 20:45:03 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week03/Weapon.kt
@@ -0,0 +1,25 @@
+package oop_00000114797_BryanValleryanAlvonso.week03
+
+class Weapon(
+    val name: String,
+) {
+    var damage: Int = 0
+        set(value){
+            if (value < 0) {
+                println("Damage tidak boleh negati!")
+                field = value
+            }
+            if (value > 1000) {
+                println("Damage tidak boleh lebih dari 1000!")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
