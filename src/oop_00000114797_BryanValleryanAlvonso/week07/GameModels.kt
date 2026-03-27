575147df09e7a67900167bbe72206f346ae6b3f6
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-03-27 15:21:30 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week07/GameModels.kt
@@ -0,0 +1,10 @@
+package oop_00000114797_BryanValleryanAlvonso.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
+
