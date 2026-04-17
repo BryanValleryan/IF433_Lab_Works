73725da7fa8e8f0601c468995e2004e90e689e26
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-04-17 15:53:46 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week08/EcommerceModels.kt
@@ -0,0 +1,16 @@
+package oop_00000114797_BryanValleryanAlvonso.week08
+
+sealed class Product {
+
+    data class Electronic(
+        val id: String,
+        val name: String,
+        val warrantyMonths: Int
+    ) : Product()
+
+    data class Clothing(
+        val id: String,
+        val name: String,
+        val size: String
+    ) : Product()
+}
