c6e7e209881b710464471b728b2954e69ba17155
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-04-24 10:27:12 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week09/TradingDashboard.kt
@@ -0,0 +1,13 @@
+package oop_00000114797_BryanValleryanAlvonso.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 10, 14.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 7, -9.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 18, 29.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 13, -8.5, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 16, -10.2, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 8, 5.9, "CLOSED")
+    )
+}
