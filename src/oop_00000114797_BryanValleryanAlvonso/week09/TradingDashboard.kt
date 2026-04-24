9aa4068a823bcfdd5d050bf20a0116af011b1c17
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-04-24 10:29:55 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_00000114797_BryanValleryanAlvonso/week09/TradingDashboard.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week09/TradingDashboard.kt
@@ -1,7 +1,6 @@
 package oop_00000114797_BryanValleryanAlvonso.week09
 
 fun main() {
-
     val tradeHistory = listOf(
         TradeLog("BTCUSDT", "LONG", 10, 14.5, "CLOSED"),
         TradeLog("ETHUSDT", "SHORT", 7, -9.2, "CLOSED"),
@@ -10,4 +9,6 @@ fun main() {
         TradeLog("BTCUSDT", "SHORT", 16, -10.2, "CLOSED"),
         TradeLog("ETHUSDT", "LONG", 8, 5.9, "CLOSED")
     )
+
+    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
 }
