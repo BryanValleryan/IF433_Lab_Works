9e98af0b96d1441d9987984102f8cd36203b6944
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-07 09:55:47 +0700

    week10: (task) populate coin repository

--- src/oop_00000114797_BryanValleryanAlvonso/week10/CryptoDashboard.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000114797_BryanValleryanAlvonso.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.82))
+    coinRepo.add(Coin("ETH", 17.67))
+    coinRepo.add(Coin("USDT", 1500.0))
+
 }
