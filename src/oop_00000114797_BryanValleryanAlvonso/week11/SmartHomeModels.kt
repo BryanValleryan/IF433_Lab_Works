5c7a1e6970bcf28df1cbc622622d0da4e53f8557
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-11 15:21:56 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000114797_BryanValleryanAlvonso.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
