37235e7984f9de118862cc7fd28e5301d09ab8e4
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-11 15:06:09 +0700

    week11: test extension functions in main

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000114797_BryanValleryanAlvonso.week11
+
+fun main() {
+    println("=== TEST EXTESNSION FUNCTION ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
