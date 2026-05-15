03e9082cab59b93c65251f41c4b6e925785ba218
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-15 10:55:14 +0700

    week12: implement multiple catch blocks

--- src/oop_00000114797_BryanValleryanAlvonso/week12/Main.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week12/Main.kt
@@ -11,4 +11,18 @@ fun main() {
     }
 
     println("Hasil parsing: $result")
+
+
+    println("=== TEST MULTIPLE CATCH ===")
+    val account = BankAccount(100.0)
+
+    try {
+        account.withdraw(150.0)
+    } catch (e: InsufficientFundsException) {
+        println("caught domain error: uang tidak cukup. ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        println("caught argument error: Input tidak valid. ${e.message}")
+    } catch (e: Exception) {
+        println("caught general error: Terjadi kesalahan tidak terduga.")
+    }
 }
