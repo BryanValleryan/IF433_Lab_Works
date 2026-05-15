1c0c8c4fe4b41e2d0efbc798bb5a5aef6dd682e2
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-15 11:08:05 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000114797_BryanValleryanAlvonso/week12/SmartFeeder.kt
@@ -0,0 +1,12 @@
+package oop_00000114797_BryanValleryanAlvonso.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    return availableGram
+}
