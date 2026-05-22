51ffde8e00d0a970b6836b874eb8453d3139b066
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-22 21:20:26 +0700

    week13: (task) implement saveTrades using use block and printWriter

--- src/oop_00000114797_BryanValleryanAlvonso/week13/CryptoTradeLogger.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week13/CryptoTradeLogger.kt
@@ -1,5 +1,7 @@
 package oop_00000114797_BryanValleryanAlvonso.week13
 
+import java.io.File
+
 data class TradeRecord(
     val id: Int,
     val symbol: String,
@@ -29,4 +31,18 @@ fun fromCsvTrade(line: String): TradeRecord? {
         )
         null
     }
+}
+
+fun saveTrades(
+    trades: List<TradeRecord>,
+    path: String
+){ File(path)
+        .printWriter()
+        .use { writer ->
+            trades.forEach {
+                writer.println(
+                    it.toCsv()
+                )
+            }
+        }
 }
