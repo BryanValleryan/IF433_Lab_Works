709fc11329fd51b97d73fe713d3c1ca35cad3ff4
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-29 16:40:45 +0700

    week14: add SeniorDiscount without modifying calculator

--- src/oop_00000114797_BryanValleryanAlvonso/week14/OcpViolation.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week14/OcpViolation.kt
@@ -1,5 +1,9 @@
 package oop_00000114797_BryanValleryanAlvonso.week14
 
-class DiscountClculator(private val strategy: DiscountStrategy) {
+class SafeDiscountClculator(private val strategy: DiscountStrategy) {
     fun calculate(price: Double) = strategy.apply(price)
+}
+
+class SeniorDiscount: DiscountStrategy {
+    override fun apply(price: Double) = price * 0.75
 }
