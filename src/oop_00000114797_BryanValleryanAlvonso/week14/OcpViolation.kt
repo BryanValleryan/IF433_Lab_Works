da6deb5eddf5ad04515d98067915f725b69295bf
BryanValleryan <bryanvalleryan0804@gmail.com>
2026-05-29 16:39:25 +0700

    week14: refactor DiscountCalculator to use strategy

--- src/oop_00000114797_BryanValleryanAlvonso/week14/OcpViolation.kt
+++ src/oop_00000114797_BryanValleryanAlvonso/week14/OcpViolation.kt
@@ -1,12 +1,5 @@
 package oop_00000114797_BryanValleryanAlvonso.week14
 
-class DiscountClculator {
-    fun calculate(price: Double, type: String): Double {
-        return when (type) {
-            "Student" -> price * 0.80
-            "member" -> price * 0.85
-            "employee" -> price * 0.70
-            else -> price
-        }
-    }
+class DiscountClculator(private val strategy: DiscountStrategy) {
+    fun calculate(price: Double) = strategy.apply(price)
 }
